f715faf266bba30dabcc8da1846dd20ee1c0869b
Janick Weberpals <jweberpals@bwh.harvard.edu>
2024-08-30 19:36:43 +0200

    worked on a more mature function to re-weight dataset in combination with matching or weighting

--- functions/match_re_weight.R
+++ functions/match_re_weight.R
@@ -1,14 +1,40 @@
-#' Custom function to match and re-weight a patient population
+#' Custom function to perform matching/weighting and re-weight a patient population
 #'
-#' @param i integer, the m_th imputed dataset (can be be used in conjunction with lapply)
-#' @param mids_object mids object with m multiple imputed datasets
-#' @param ps_formula formula of propensity score model fit
+#' @details This function is a wrapper for \code{\link[MatchIt]{matchit}} 
+#' and \code{\link[WeightIt]{weighit}}in combination with 
+#' \code{\link[anesrake]{anesrake}}.
+#' 
+#' The function performs any matching algorithm supplied 
+#' in \code{\link[MatchIt]{matchit}} or any weighting 
+#' algorithm in \code{\link[WeightIt]{weighit}}.
+#' 
+#' If nothing is specified for \code{targets}, the function will simply return 
+#' the \code{\link[MatchIt]{matchit}} or \code{\link[WeightIt]{weighit}} object.
+#' 
+#' If a list of target distributions is specified for \code{targets}, 
+#' the function will perform a corresponding re-weighting of matched 
+#' patients or patients with weights greater than 0, respectively.
+#' 
+#' In case of already weighted datasets (e.g., via propensity score-derived weights),
+#' those weights are accounted for (\code{weightvec} argument) in the
+#' \code{\link[anesrake]{anesrake}} call.
+#' 
+#' @param x data.frame or mild object/list of data.frames if used in combination with lapply
+#' @param targets named list of all target values for the raking procedure (see \code{\link[anesrake]{anesrake}})
+#' @param method character, one of "matching" or "weighting"
+#' @param ... other arguments and specifications to pass on to \code{\link[MatchIt]{matchit}} or \code{\link[WeightIt]{weighit}}, depending on chosen \code{method}
 #' 
 #' @importFrom mice complete
 #' @importFrom MatchIt matchit match.data
-#' @importFrom WeightIt weightit
+#' @importFrom WeightIt weightit as.weightit
 #' 
-#' @return matched and re-weighted dataset
+#' @return an object of type \code{matchit} or \code{weightit} with
+#' included sampling weights (\code{s.weights}) if re-weighting was performed.
+#' 
+#' @seealso 
+#' \code{\link[MatchIt]{matchit}}
+#' \code{\link[WeightIt]{weighit}}
+#' \code{\link[anesrake]{anesrake}}
 #' 
 #' @export
 #'
@@ -21,119 +47,179 @@
 #'   imposeNA = TRUE
 #'   )
 #'   
+#'  # impute data
+#'  data_imp <- futuremice(
+#'    parallelseed = 42,
+#'    n.core = 7,
+#'    data = data_miss,
+#'    method = "rf",
+#'    m = 10,
+#'    print = FALSE
+#'    )
+#'    
+#'  # define covariates for propensity score model
 #'  covariates <- data_miss |> 
 #'    select(starts_with("c_"), starts_with("dem_")) |> 
 #'    colnames()
+#'  
+#'  # define propensity score model
+#'  ps_form <- as.formula(paste("treat ~", paste(covariates, collapse = " + ")))
+#'  
+#'  # create a mild object containing lists of data.frames
+#'  data_mild <- mice::complete(data = data_imp, action = "all", include = FALSE)
+#'  
+#'  # define target distribution for smoking (64%) vs. never smoking (36%)
+#'  smoker_target <- c(.64, 1-.64)
+#'  
+#'  # summarize target distributions in a named list vector --------------
+#'  targets <- list(smoker_target)
+#'  names(targets) <- c("c_smoking_history")
+#'  
+#'  # create a mild object containing lists of data.frames
+#'  data_mild <- mice::complete(data = data_imp, action = "all", include = FALSE)
+#'  
+#'  # call match re-weight
+#'  matchit_out_list <- lapply(
+#'   X = data_mild, 
+#'   FUN = match_re_weight,
+#'   targets = targets,
+#'   # arguments passed on to matchit
+#'   formula = ps_form,
+#'   ratio = 1,
+#'   method = "nearest",
+#'   distance = "glm",
+#'   link = "logit",
+#'   caliper = 0.01,
+#'   replace = F
+#'   )
+#'   
+#'  # convert the output back into a mimids object
+#'  data_mimids_from_function <- MatchThem::as.mimids(
+#'    x = matchit_out_list, 
+#'    datasets = mids_object
+#'    )
 #' 
 
-match_re_weight <- function(i, 
-                            mids_object = NULL, 
-                            ps_formula = NULL,
+match_re_weight <- function(x,
+                            targets = NULL,
+                            method = NULL,
                             ...
                             ){
   
-  # perform matching on the mth imputed dataset -----------------------------
+  # checks
+  assertthat::assert_that(inherits(x, c("data.frame", "list")), msg = "<x> needs to be a data frame or a list of data frames")
+  assertthat::assert_that(method %in% c("matching", "weighting"), msg = "<method> needs to be either matching or weighting")
   
-  # extract the mth imputed dataset
-  imputed_data_m <- mice::complete(data = mids_object, action = i)
+  if(is.null(targets)) message("No target distributions specified, no re-weighting will be performed.")
   
-  # create a matchit object
-  matchit_out <- MatchIt::matchit(
-    formula = ps_formula,
-    data = imputed_data_m,
-    ...
-    )
-  
-  # read data ---------------------------------------------------------------
-  
-  # get ALL patients with matching weights
-  data_matchit_all <- MatchIt::match.data(
-    object = matchit_out, 
-    drop.unmatched = FALSE
-    )
-  
-  # create a data frame with temporary patient/case ids
-  data_matchit_all <- data_matchit_all |> 
-    # create a case id
-    mutate(caseid = seq(1, nrow(data_matchit_all), 1)) |> 
-    # convert sex and ecog into a factor variable since anesrake doesn't accept 0/1 numeric encoding
-    mutate(across(c(dem_sex_cont, c_ecog_cont), function(x) factor(as.character(x))))
-  
-  # create a sub-cohort with matching weights == 1 (= matched patients)
-  # we will compute the new sampling/re-weighting weights only on matched patients
-  data_matched <- data_matchit_all |> 
-    dplyr::filter(weights == 1)
-  
-  # Define FLAURA distributions for key covariates --------------------------
-  # order is as in Table 1
+  # perform matching on the imputed dataset -----------------------------
+  if(method == "matching"){
+    
+    # create a matchit object on x
+    # if no re-weighting is desired
+    # this is already it
+    object_out <- MatchIt::matchit(
+      data = x,
+      ...
+      )
+    
+    # get ALL patients with matching weights
+    data_all <- MatchIt::match.data(
+      object = object_out, 
+      drop.unmatched = FALSE
+      )
+    
+    # create temporary patient/case ids for raking procedure
+    data_all <- data_all |> 
+      mutate(caseid = seq(1, nrow(data_all), 1)) |> 
+      # indicator for patients who re-weighting should be applied to
+      # = matched patients
+      mutate(reweight = weights)
+    
+  }else if(method == "weighting"){
+    
+    # create a matchit object on x
+    # if no re-weighting is desired
+    # this is already it
+    object_out <- WeightIt::weightit(
+      data = x,
+      ...
+      )
+    
+    # get ALL patients with weights
+    data_all <- x |> 
+      dplyr::mutate(weights = object_out$weights) |> 
+      mutate(caseid = seq(1, nrow(x), 1)) |> 
+      # indicator for patients who re-weighting should be applied to
+      # = patients with weight > 0
+      mutate(reweight = ifelse(weights == 0, 0, 1))
+    
+  }
 
-  ## sex ---------------------------------------------------------------------
-  
-  # male sex by exposure
-  avg_prop_male <- sum(.36, .38)/2
-  
-  # female (0) to male (1) proportion:
-  sex_target <- c(1-avg_prop_male, avg_prop_male) 
-  names(sex_target) <- c("0", "1")
-
-  ## race --------------------------------------------------------------------
-  # white, asian, other
-  race_target <- c(.36, .62, .02) # "other" is rounded up to 2% to sum up to 100%
-  names(race_target) <- c("White", "Asian", "Other")
-
-  ## smoking -----------------------------------------------------------------
-  
-  # never smoking by exposure
-  avg_prop_never_smoker <- sum(.65, .63)/2
-  
-  # current/former smoker (TRUE) to never smoker (FALSE) proportion
-  # note: logical variables in dataframe can be matched to a numeric vector of length 2 and ordered with the TRUE target as the first element and the FALSE target as the second element.
-  smoker_target <- c(1-avg_prop_never_smoker, avg_prop_never_smoker)
-
-  ## ecog --------------------------------------------------------------------
-  
-  # ecog 0 by exposure 
-  avg_prop_ecog0 <- sum(.4, .42)/2
-  
-  # ecog 0 to ecog 1 proportion
-  ecog_target <- c(avg_prop_ecog0, 1-avg_prop_ecog0)
-  names(ecog_target) <- c("0", "1")
   
 
-  # summarize target distributions in a named list vector --------------
-  targets <- list(sex_target, race_target, smoker_target, ecog_target)
-  names(targets) <- c("dem_sex_cont", "dem_race", "c_smoking_history", "c_ecog_cont")
+  # if re-weighting by the anesrake procedure is desired: -------------------
+  if(!is.null(targets)){
   
-
-  # apply anesrake function to re-weight -----------------------------------
-  anesrake_out <- anesrake::anesrake(
-    inputter = targets, 
-    dataframe = data_matched, 
-    caseid = data_matched$caseid,
-    weightvec = data_matched$weights,
-    verbose = FALSE
-    )
+    # apply anesrake function to re-weight -----------------------------------
+    anesrake_out <- anesrake::anesrake(
+      # target distributions for raking procedure
+      inputter = targets, 
+      # data to be weighted
+      dataframe = data_all, 
+      # patient identifier
+      caseid = data_all$caseid,
+      # other weights that should be accounted for before re-weighting is conducted
+      # for matching this is 0 (unmatched) and 1 (matched) and for weighting this is the actual weights
+      weightvec = data_all$weights,
+      # we only want to re-weight patients who were matched/ have weights > 0
+      filter = data_all$reweight,
+      # no verbosity
+      verbose = FALSE
+      )
+    
+    # create a temporary dataframe with id and sampling/re-weighting weights
+    caseweights <- data.frame(caseid = anesrake_out$caseid, re_weights = anesrake_out$weightvec)
+    
+    # join back to matchit dataframe with ALL subjects
+    # this is necessary to add the sampling weights to the final 
+    # matchit object in the next step where all patients are required
+    # unmatched patients get a sampling weight of 0
+    data_all <- data_all |> 
+      dplyr::left_join(caseweights, by = "caseid") |> 
+      # unmatched patients or patients with weight = 0 get a sampling/re-weighting weight of 0
+      dplyr::mutate(re_weights = ifelse(is.na(re_weights), 0, re_weights)) |>
+      # make sure we have the same original order of subject
+      dplyr::arrange(caseid) |> 
+      # now we can drop the temporary id
+      dplyr::select(-caseid)
+    
+    if(method == "matching"){
+      
+      # add sampling weights to the matchit object so that 
+      # they are incorporated into balance assessment 
+      # and creation of the weights
+      object_out <- MatchIt::add_s.weights(
+        m = matchit_out,
+        s.weights = data_all$re_weights
+        )
+      
+    }else if(method == "weighting"){
+      
+      object_out <- WeightIt::as.weightit(
+        x = weighit_out$weights,
+        covs = data_all,
+        treat = data_all$treat,
+        s.weights = data_all$re_weights
+        ) 
+      
+      warning("<covariates> do not represent those that were used for weighting but all available covariates.")
+      
+      }
+    
+    }
   
-  # create a temporary dataframe with id and sampling/re-weighting weights
-  caseweights <- data.frame(caseid = anesrake_out$caseid, re_weights = anesrake_out$weightvec)
-  
-  # join back to matchit dataframe with ALL subjects
-  # this is necessary to add the sampling weights to the final matchit object
-  data_matchit_all <- data_matchit_all |> 
-    dplyr::left_join(caseweights, by = "caseid") |> 
-    # unmatched patients get a sampling/re-weighting weight of 0
-    dplyr::mutate(re_weights = ifelse(is.na(re_weights), 0, re_weights)) |>
-    # make sure we have the same original order of subject
-    dplyr::arrange(caseid) |> 
-    # now we can drop the temporary id
-    dplyr::select(-caseid)
-  
-  matchit_out <- MatchIt::add_s.weights(
-    m = matchit_out,
-    s.weights = data_matchit_all$re_weights
-    )
-  
-  return(matchit_out)
+  return(object_out)
   
 }
 
